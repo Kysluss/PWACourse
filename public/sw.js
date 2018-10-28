c493b513e7fcdd032353a362a45410bca1faa452
unknown <Kysluss@aol.com>
2018-10-28 17:23:38 -0400

    Added background synchronization code and installed firebase

--- public/sw.js
+++ public/sw.js
@@ -185,3 +185,42 @@ self.addEventListener('fetch', function(event) {
 //     fetch(event.request)
 //   );
 // });
+
+self.addEventListener('sync', function(event) {
+  console.log('[Service Worker] Background syncing', event);
+
+  if(event.tag === 'sync-new-post') {
+    console.log('[Service Worker] Syncing new posts');
+
+    event.waitUntil(
+      readAllData('sync-posts')
+        .then(function(posts) {
+          for(var dt of posts) {
+            fetch('https://pwagram-d96a0.firebaseio.com/posts.json', {
+              method: 'POST', 
+              headers: {
+                'Content-Type': 'application/json', 
+                'Accept': 'application/json'
+              }, 
+              body: JSON.stringify({
+                id: dt.id, 
+                title: dt.title, 
+                location: dt.location, 
+                image: 'https://firebasestorage.googleapis.com/v0/b/pwagram-d96a0.appspot.com/o/sf-boat.jpg?alt=media&token=6ccea2d0-90d6-47b3-8d64-3e5ef903aa00'
+              })
+            })
+            .then(function(res) {
+              console.log('Sent data', res);
+
+              if(res.ok) {
+                deleteItemFromData('sync-posts', dt.id);
+              }
+            })
+            .catch(function(err) {
+              console.log('Error while sending data', err);
+            })
+          }
+        })
+    );
+  }
+});
