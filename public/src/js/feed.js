5a534652a0cf0b332267d770cffebdbb98241367
unknown <Kysluss@aol.com>
2018-12-09 15:34:19 -0500

    Added workbox

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -10,7 +10,47 @@ var canvasElement = document.querySelector('#canvas');
 var captureButton = document.querySelector('#capture-btn');
 var imagePicker = document.querySelector('#image-picker');
 var imagePickerArea = document.querySelector('#pick-image');
-var picture = null;
+var picture = { lat: 0, lng: 0 };
+var locationBtn = document.querySelector('#location-btn');
+var locationLoader = document.querySelector('#location-loader');
+var fetchedLocation;
+
+locationBtn.addEventListener('click', function(event) {
+  if(!('geolocation' in navigator)) return;
+
+  var sawAlert = false;
+
+  // Hide the button and show the spinner
+  locationBtn.style.display = 'none';
+  locationLoader.style.display = 'block';
+  // Get the current position
+  navigator.geolocation.getCurrentPosition(function(position) {
+    locationBtn.style.display = 'inline';
+    locationLoader.style.display = 'none';
+    // You could take the latitude and longitude
+    // Then pass this to Googles Geolocation API to get a city name
+    fetchedLocation = { lat: position.coords.latitude, lng: position.coords.longitude };
+    locationInput.value = 'At home';
+    document.querySelector('#manual-location').classList.add('is-focused');
+  }, function(err) {
+    // An error happened
+    console.log(err);
+    // Enable button and disable loader
+    locationBtn.style.display = 'inline';
+    locationLoader.style.display = 'none';
+    if(!sawAlert) {
+      sawAlert = true;
+      alert('Couldn\'t fetch location, please enter a manual location');
+    }
+    fetchedLocation = { lat: 0, lng: 0 };
+  }, { timeout: 7000 });
+});
+
+function initializeLocation() {
+  if(!('geolocation' in navigator)) {
+    locationBtn.style.display = 'none';
+  }
+}
 
 function initializeMedia() {
   if(!('mediaDevices' in navigator)) {
@@ -50,6 +90,13 @@ captureButton.addEventListener('click', function(event) {
   picture = dataURItoBlob(canvasElement.toDataURL());
 });
 
+// If the user clicks and picks an image because they have no camera,
+// We need to se the picture that will be submitted
+// Because we only allow selecting of 1 image, files[0] will give us what they selected
+imagePicker.addEventListener('change', function(event) {
+  picture = event.target.files[0];
+});
+
   // At this point, navigator.mediaDevices.getUserMedia will exist
   // It will return a Promise which keeps it in line with the standard
   // This will automatically ask the user if the app can use the video
@@ -71,8 +118,11 @@ captureButton.addEventListener('click', function(event) {
 function openCreatePostModal() {
   // createPostArea.style.display = 'block';
   // setTimeout(function() {
-    createPostArea.style.transform = 'translateY(0)';
+    setTimeout(function() {
+      createPostArea.style.transform = 'translateY(0)';
+    }, 1);
     initializeMedia();
+    initializeLocation();
   // }, 1);
   if (deferredPrompt) {
     deferredPrompt.prompt();
@@ -101,7 +151,6 @@ function openCreatePostModal() {
 }
 
 function closeCreatePostModal() {
-  createPostArea.style.transform = 'translateY(100vh)';
   // Cleanup of the video area
   // If a user removes permission for media
   // In that case, hiding these things will keep a consistent UX
@@ -109,6 +158,20 @@ function closeCreatePostModal() {
   videoPlayer.style.display = 'none';
   canvasElement.style.display = 'none';
   // createPostArea.style.display = 'none';
+  locationBtn.style.display = 'none';
+  locationLoader.style.display = 'inline';
+  captureButton.style.display = 'inline';
+
+  if(videoPlayer.srcObject) {
+    videoPlayer.srcObject.getVideoTracks().forEach(function(track) {
+      track.stop();
+    });
+  }
+
+  // Added here to avoid the clunky looking animation out
+  setTimeout(function() {
+    createPostArea.style.transform = 'translateY(100vh)';
+  }, 1);
 }
 
 shareImageButton.addEventListener('click', openCreatePostModal);
@@ -199,6 +262,8 @@ function sendData() {
   postData.append('id', id);
   postData.append('title', titleInput.value);
   postData.append('location', locationInput.value);
+  postData.append('rawLocationLat', rawLocation.lat);
+  postData.append('rawLocationLng', rawLocation.lng);
   postData.append('file', picture, id + '.png');
 
   fetch('https://us-central1-pwagram-d96a0.cloudfunctions.net/storePostData', {
@@ -228,7 +293,8 @@ form.addEventListener('submit', function(event) {
           id: new Date().toISOString(),
           title: titleInput.value,
           location: locationInput.value, 
-          picture: picture
+          picture: picture, 
+          rawLocation: fetchedLocation 
         };
         writeData('sync-posts', post)
           .then(function() {
